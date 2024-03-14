01c8d879acb471c0f309a7c37b7d292c03f486d2
Tkarkib Lhassan <73590332+LhassanTkarkib@users.noreply.github.com>
2024-03-14 10:34:30 +0000

    created a new gateway that impliments keycloack

---
+++ Gateway/src/test/java/com/socialnetworks/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socialnetworks.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
